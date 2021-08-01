fd984440d7a377d9ec276dafc57c21165286bd61
Donghyeon Min <mindonghyeon890@gmail.com>
2021-08-02 01:12:01 +0900

    initial commit

---
+++ src/main/java/com/example/dddboard/DddBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dddboard/DddBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
